a23bae8aa1279c8d2398ee33a9adc1ad1d696523
Boderio <boderiotrigan@hotmail.com>
2024-10-09 12:00:21 +0200

    cours 4 - insertion et flash

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Form\AnnonceType;
 use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
@@ -31,10 +33,34 @@ class AdController extends AbstractController
     }
 
     #[Route("/ads/new", name:"ads_create")]
-    public function create(): Response
+    public function create(Request $request, EntityManagerInterface $manager): Response
     {
         $ad = new Ad();
+
+        //$arrayForm = $request->request->all();
+
         $form = $this->createForm(AnnonceType::class, $ad);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            //dump($arrayForm['annonce']);
+            $manager->persist($ad);
+            $manager->flush();
+
+            // message flash
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>".$ad->getTitle()."</strong> a bien été enregristrée!"
+            );
+
+            // redirection vers l'annonce en elle-même
+            return $this->redirectToRoute('ads_show',[
+                'slug' => $ad->getSlug()
+            ]);
+
+        }
+
 
         return $this->render("ad/new.html.twig",[
             'myForm' => $form->createView()
