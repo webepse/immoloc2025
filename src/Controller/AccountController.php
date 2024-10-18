067f7dab241fe54ab03a75bd6ec134bdc8925365
berti <berti@CLASSES.LOCAL>
2024-10-18 13:54:15 +0200

    cours 7 - édition user

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,14 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\AccountType;
 use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Exception\TooManyLoginAttemptsAuthenticationException;
 
 class AccountController extends AbstractController
@@ -81,6 +82,34 @@ class AccountController extends AbstractController
 
         return $this->render("account/registration.html.twig",[
             'myForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet de modifier son profil
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route("/account/profile", name:"account_profile")]
+    public function profile(Request $request, EntityManagerInterface $manager): Response
+    {
+        $user = $this->getUser(); // récup le user connecté
+        $form = $this->createForm(AccountType::class,$user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($user);
+            $manager->flush();
+            $this->addFlash(
+            'success',
+            "Les données ont été enregistrées avec succès"
+            );
+        }
+
+        return $this->render("account/profile.html.twig",[
+            "myForm" => $form->createView()
         ]);
     }
 }
---
+++ src/Form/AccountType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Form\ApplicationType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class AccountType extends ApplicationType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, $this->getConfiguration("Prénom","Votre prénom"))
+            ->add('lastName', TextType::class, $this->getConfiguration("Nom","Votre nom de famille"))
+            ->add('email', EmailType::class, $this->getConfiguration("Email","Votre adresse E-mail"))
+            ->add('picture', UrlType::class, $this->getConfiguration("Photo de profil","Url de votre avatar..."))
+            ->add('introduction', TextType::class, $this->getConfiguration("Introduction","Orésenation rapide"))
+            ->add('description', TextareaType::class, $this->getConfiguration("Description détaillée","Présentez vous avec un peu plus de détails"))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
