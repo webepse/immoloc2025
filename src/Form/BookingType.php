14789ac12b55389082867efe9ef9b3cfb948bdf1
Boderio <boderiotrigan@hotmail.com>
2024-11-20 11:15:26 +0100

    cours 12 - Les réservations datepicker + DataTransformer

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -6,26 +6,33 @@ use App\Entity\Ad;
 use App\Entity\User;
 use App\Entity\Booking;
 use App\Form\ApplicationType;
+use App\Form\DataTransformer\FrenchToDateTimeTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BookingType extends ApplicationType
 {
+    public function __construct(
+        private FrenchToDateTimeTransformer $transformer
+    ){}
+
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('startDate', DateType::class, $this->getConfiguration("Date d'arrivée","La date à laquelle vous comptez arriver",[
-                'widget' => "single_text"
-            ]) )
-            ->add('endDate', DateType::class, $this->getConfiguration("Date de départ", "La date à laquelle vous comptez partir du bien réservé",[
-                'widget' => "single_text"
-            ]))
+            ->add('startDate', TextType::class, $this->getConfiguration("Date d'arrivée","La date à laquelle vous comptez arriver"))
+            ->add('endDate', TextType::class, $this->getConfiguration("Date de départ", "La date à laquelle vous comptez partir du bien réservé"))
             ->add('comment', TextareaType::class, $this->getConfiguration(false, "Si vous avez un commentaire, n'hésitez pas à en fare part",[
                 "required" => false
             ]))
         ;
+
+        $builder->get('startDate')->addModelTransformer($this->transformer);
+        $builder->get('endDate')->addModelTransformer($this->transformer);
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
