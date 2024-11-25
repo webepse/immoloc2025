3d82db05afd9cfc36c422bccae8957de88943b00
Boderio <boderiotrigan@hotmail.com>
2024-11-25 10:09:09 +0100

    cours 13 - gestion des commentaire - formulaire

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Ad;
 use App\Entity\Booking;
+use App\Entity\Comment;
 use App\Form\BookingType;
+use App\Form\CommentType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -59,14 +61,32 @@ class BookingController extends AbstractController
      * Permet d'afficher la page d'une réservation
      *
      * @param Booking $booking
+     * @param Request $request
+     * @param EntityManagerInterface $manager
      * @return Response
      */
     #[Route("/booking/{id}", name:"booking_show")]
     #[IsGranted("ROLE_USER")]
-    public function show(Booking $booking): Response
+    public function show(Booking $booking, Request $request, EntityManagerInterface $manager): Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $comment->setAd($booking->getAd())
+                ->setAuthor($this->getUser());
+
+            $manager->persist($comment);
+            $manager->flush();
+
+            $this->addFlash('success',"Votre commentaire a bien été pris en compte");
+        }
+
         return $this->render("booking/show.html.twig",[
-            'booking' => $booking
+            'booking' => $booking,
+            'myForm' => $form->createView()
         ]);
     }
 }
