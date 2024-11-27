b2af8efde59b1dbedc14104a257c032f75661b9a
Boderio <boderiotrigan@hotmail.com>
2024-11-27 11:56:44 +0100

    cours 15 - administration des commentaires

---
+++ src/Controller/AdminCommentController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Form\CommentType;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminCommentController extends AbstractController
+{
+    /**
+     * Permet d'afficher les commentaires
+     *
+     * @param CommentRepository $repo
+     * @return Response
+     */
+    #[Route('/admin/comments', name: 'admin_comments_index')]
+    public function index(CommentRepository $repo): Response
+    {
+        return $this->render('admin/comment/index.html.twig', [
+            'comments' => $repo->findAll()
+        ]);
+    }
+
+        
+    /**
+     * Permet d'éditer un commentaire
+     *
+     * @param Comment $comment
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route("admin/comments/{id}/edit", name: "admin_comments_edit")]
+    public function edit(Comment $comment, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form= $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($comment);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Le commentaire n°<strong>".$comment->getId()."</strong> a bien été modifié"
+            );
+
+            return $this->redirectToRoute("admin_comments_index");
+        }
+
+        return $this->render("admin/comment/edit.html.twig",[
+            'comment' => $comment,
+            'myForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet de supprimer un commentaire
+     *
+     * @param Comment $comment
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route("admin/comments/{id}/delete", name: "admin_comments_delete")]
+    public function delete(Comment $comment, EntityManagerInterface $manager): Response
+    {
+        $this->addFlash(
+            'success',
+            "Le commentaire n°".$comment->getId()." a bien été supprimé"
+        );
+        $manager->remove($comment);
+        $manager->flush();
+        return $this->redirectToRoute("admin_comments_index");
+    }
+}
